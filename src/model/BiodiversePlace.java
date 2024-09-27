00b78dbc76deab0f8242667347ea8ed68ef44537
stien95 <stivenvillegas258@gmail.com>
2024-09-26 23:14:04 -0500

    adding toString method to biodiversePlace class

--- src/model/BiodiversePlace.java
+++ src/model/BiodiversePlace.java
@@ -6,54 +6,72 @@ public class BiodiversePlace {
     private String name;
     private double area;
     private String photo;
-    private Date inagurationDate;
+    private Date inaugurationDate;
     private double requiredFunding;
     private Department department;
     // caretakerCommunity, residentSpecies.
-    
-    public BiodiversePlace(String name, double area, String photo, Date inagurationDate, double requiredFunding, Department department) {
+
+    public BiodiversePlace(String name, double area, String photo, Date inaugurationDate, double requiredFunding,
+            Department department) {
         this.name = name;
         this.area = area;
         this.photo = photo;
-        this.inagurationDate = inagurationDate;
+        this.inaugurationDate = inaugurationDate;
         this.requiredFunding = requiredFunding;
         this.department = department;
     }
+
     String getName() {
         return name;
     }
+
     void setName(String name) {
         this.name = name;
     }
+
     double getArea() {
         return area;
     }
+
     void setArea(double area) {
         this.area = area;
     }
+
     String getPhoto() {
         return photo;
     }
+
     void setPhoto(String photo) {
         this.photo = photo;
     }
-    Date getInagurationDate() {
-        return inagurationDate;
+
+    Date getInaugurationDate() {
+        return inaugurationDate;
     }
-    void setInagurationDate(Date inagurationDate) {
-        this.inagurationDate = inagurationDate;
+
+    void setInaugurationDate(Date inaugurationDate) {
+        this.inaugurationDate = inaugurationDate;
     }
+
     double getRequiredFunding() {
         return requiredFunding;
     }
+
     void setRequiredFunding(double requiredFunding) {
         this.requiredFunding = requiredFunding;
     }
+
     Department getDepartment() {
         return department;
     }
+
     void setDepartment(Department department) {
         this.department = department;
     }
-}
 
+    public String toString() {
+        return "Lugar biodiverso: " + name + "\nArea: " + area + "\nFecha de inauguración: "
+                + inaugurationDate.toString()
+                + "\nFinanciamiento requerido: " + requiredFunding + "\nDepartamento: " + department.getName();
+    }
+}
